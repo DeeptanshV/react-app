5a5ed4649fc6d8c6fa1bf775863a589937369fbd
Deeptansh Verma <75324676+DeeptanshV@users.noreply.github.com>
2023-07-04 20:22:10 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with Deeptansh
+          Learn React - beginner stage
+            
         </a>
       </header>
     </div>
