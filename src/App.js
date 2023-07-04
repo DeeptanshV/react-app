47a165f9aaf8c28df010f53be92d63ea18a28875
Deeptansh Verma <deeptansh2996@gmail.com>
2023-07-04 12:41:06 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React with Deeptansh Verma
         </a>
       </header>
     </div>
