3d0d5399bf4f61b2f3012544189553811d0a7b77
Deeptansh Verma <deeptansh2996@gmail.com>
2023-07-04 12:43:45 +0530

    added

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with Deeptansh Verma
+          Learn React with Deeptansh
         </a>
       </header>
     </div>
